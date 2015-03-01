69b62f70d9c4593bf67a5f4dfbb5e4ec76a22c2c
sagivo <sagiv4@gmail.com>
2015-02-28 22:23:35 -0800

    a

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
   //console.log(`Node app is running at localhost: ${ app.get('port') }`);
-  console.log("Node app is running at localhost: " + ${ app.get('port') });
+  console.log("Node app is running at localhost: " + app.get('port'));
 });
 
