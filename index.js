dadfb642656d98a310d2c5fd545d6c6808b27361
sagivo <sagiv4@gmail.com>
2015-02-28 22:22:49 -0800

    a

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
-  console.log(`Node app is running at localhost: ${ app.get('port') }`);
+  //console.log(`Node app is running at localhost: ${ app.get('port') }`);
+  console.log("Node app is running at localhost: " + ${ app.get('port') });
 });
 
